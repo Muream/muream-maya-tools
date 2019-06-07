32935c3f16cb2a87a152dd78f3f5ee8cdce256c5
Loïc Pinsard <muream@gmail.com>
2019-06-07 22:33:49 +0200

    fix shelf import and add vendor to python path

--- mmtk/__init__.py
+++ mmtk/__init__.py
@@ -1,14 +1,21 @@
 import logging
 import os
+import sys
+
 import maya.cmds as cmds
-import mmtk.common.shelf as shelf
 
 
 logger = logging.getLogger(__name__)
 
 
 def setup():
+    # add the vendored dir to the python path
+    vendor_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'vendor'))
+    sys.path.append(vendor_dir)
+    
+    # create the shelf
     shelf_icon_path = os.path.join(os.path.dirname(__file__), 'common', 'shelf', 'icons')
-    cmds.evalDeferred("shelf.ShelfMMTK('MMTK', icon_path='{}/')".format(shelf_icon_path))
+    cmds.evalDeferred("import mmtk.common.shelf as shelf; shelf.ShelfMMTK('MMTK', icon_path='{}/')".format(shelf_icon_path))
+
     logger.info("Setup for Muream Maya Toolkit done.")
 
