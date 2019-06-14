2e173792e302e8ee7ba8cf080544603283303286
Muream <muream@gmail.com>
2019-06-15 00:33:20 +0200

    Change Icon path and refactor mmtk/__init__.py

--- mmtk/__init__.py
+++ mmtk/__init__.py
@@ -1,25 +1,49 @@
 import logging
 import os
-import sys
+import site
 
 import maya.cmds as cmds
 from maya.utils import executeDeferred
 from .default import init_defaults
+import mmtk.common.shelf as shelf
 
 
 logger = logging.getLogger(__name__)
 
 
 def setup():
-    # add the vendored dir to the python path
-    vendor_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'vendor'))
-    sys.path.append(vendor_dir)
-    
-    # create the shelf
-    # shelf_icon_path = os.path.join(os.path.dirname(__file__), 'common', 'shelf', 'icons')
-    # cmds.evalDeferred("import mmtk.common.shelf as shelf; shelf.ShelfMMTK('MMTK', icon_path='{}/')".format(shelf_icon_path))
-
+    add_vendors_to_path()
+    add_cosmos_action_path()
+    executeDeferred(create_mmtk_shelf)
     executeDeferred(init_defaults)
 
     logger.info("Setup for Muream Maya Toolkit done.")
 
+
+def add_vendors_to_path():
+    vendor_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', 'vendor')
+    )
+
+    site.addsitedir(vendor_dir)
+
+
+def add_cosmos_action_path():
+    try:
+        import cosmos
+    except:
+        pass
+    else:
+        cosmos_actions_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), '..', 'cosmos-actions')
+        )
+        path_string = cosmos.prefs.getGenericSettings("scriptPath")
+        path_string += ";" + cosmos_actions_path
+        cosmos.prefs.writegenericSettings("scriptPath", path_string)
+
+
+def create_mmtk_shelf():
+    shelf_icon_path = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', 'icons')
+    )
+    shelf.ShelfMMTK('MMTK', icon_path=shelf_icon_path)
