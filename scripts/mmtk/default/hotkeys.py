ef0c06f8e587f9738fb3d1fdbfd25b9eb4e809e0
Loïc Pinsard <muream@gmail.com>
2020-12-16 21:36:14 +0000

    fix hotkeys loading.

--- scripts/mmtk/default/hotkeys.py
+++ scripts/mmtk/default/hotkeys.py
@@ -5,5 +5,5 @@ import maya.cmds as cmds
 def init_hotkeys():
     if not "hotkeys" in cmds.hotkeySet(q=True, hotkeySetArray=True):
         cmds.hotkeySet(
-            edit=True, ip=os.path.join(os.path.basename(__file__), "hotkeys.mhk")
+            edit=True, ip=os.path.join(os.path.dirname(__file__), "hotkeys.mhk")
         )
